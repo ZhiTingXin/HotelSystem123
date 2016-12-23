db424ef0065299cc6ddca85ea96f776bf3e20713
TraxexC <1156489606@qq.com>
2016-12-23 23:31:21 +0800

    add system satff

---
+++ HotelSystem_client/src/VO/Cities.java
@@ -0,0 +1,29 @@
+package VO;
+
+public class Cities {
+
+	final String[] cities = { "北京", "南京", "上海", "重庆", "郑州", "武汉", "长沙", "广州", "台北", "香港" };
+	
+	final String[] districtsBJ = {"王府井","崇文门","公主坟","三里屯"};
+	final String[] districtsNJ = {"新街口","鼓楼","仙林","应天大街"};
+	final String[] districtsSH = {"徐家汇","陆家嘴","南京西路","淮海路"};
+	final String[] districtsCQ = {"解放碑","观音桥","沙坪坝","杨家坪"};
+	final String[] districtsZZ = {"二七","花园路","北环","碧沙岗"};
+	final String[] districtsWH = {"武广","江汉路","武胜路","菱角湖"};
+	final String[] districtsCS = {"","","",""};
+	final String[] districtsGZ = {};
+	final String[] districtsTB = {};
+	final String[] districtsXG = {};
+	
+//	public String[] getDistricts(String city) {
+//		String[] districts;
+//		switch (city) {
+//		case "北京":
+//			return districtsBJ;
+//			break;
+//
+//		default:
+//			break;
+//		}
+//	}
+}
